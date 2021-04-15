e6124409ffd43995fe45b1cbf7c986135b9f36cc
Gaven Rudiak <gavenpaul12@gmail.com>
2021-04-14 20:27:57 -0400

    Basics are done

--- Variables.py
+++ Variables.py
@@ -18,3 +18,78 @@ meal = "Steak"
 # Printing out dinner
 print("Dinner:")
 print(meal)
+
+# Numbers
+# Define the release and runtime integer variables below:
+release_year = 1999
+runtime = 78
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = 7.5
+
+#calculations
+print(25 * 68 + 13 / 28)
+
+quilt_width = 8
+quilt_length = 8
+print(quilt_width * quilt_length)
+
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2)
+# 7x7 quilt
+print(7 ** 2)
+# 8x8 quilt
+print(8 ** 2)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print(6 ** 4)
+
+# pring remainder of a division
+my_team = 27 % 4
+print(my_team)
+
+#string concatination
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+message = string1+string2+string3+string4+string5+string6
+
+print(message)
+
+#addition with variables
+total_price = 0
+
+new_sneakers = 50.00
+
+total_price += new_sneakers
+
+nice_sweater = 39.00
+fun_books = 20.00
+# Update total_price here:
+total_price += nice_sweater 
+total_price += fun_books
+
+print("The total price is", total_price)
+
+#multi-Line string
+# Assign the string here
+to_you = """
+Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?
+"""
+
+
+print(to_you)
+
+#overall
+my_age = 17
+half_my_age = 17/2
+greeting = "Hello"
+name = "Gaven"
+greeting_with_name = greeting + name
